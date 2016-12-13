96734d092b9bcd2f0411e2428a51a8b3464ed789
Lombardo <c_anlomb@qualcomm.com>
2016-12-13 15:59:09 -0800

    Added Unit test coverage to DatabaseEventService

--- src/test/java/com/pwc/core/framework/service/DatabaseEventServiceTest.java
+++ src/test/java/com/pwc/core/framework/service/DatabaseEventServiceTest.java
@@ -14,20 +14,21 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
+import java.math.BigDecimal;
 import java.sql.*;
 import java.util.List;
+import java.util.UUID;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class DatabaseServiceTest {
+public class DatabaseEventServiceTest {
 
     final String SELECT_STATEMENT = "select name from model_recommendation where id=?";
+    final String SELECT_LIKE_STATEMENT = "select name from model_recommendation where name like ?";
+    final String SELECT_GENERIC_STATEMENT = "select name from model_recommendation where name field = ?";
     final String UPDATE_STATEMENT = "update patent_analysis set resubmit_to_model=? where id=?";
-
-    @Mock
-    private ResultSet mockResultSet;
 
     @Mock
     JdbcTemplate mockJdbcTemplate;
@@ -40,6 +41,9 @@ public class DatabaseServiceTest {
 
     @Mock
     PreparedStatement mockPreparedStatement;
+
+    @Mock
+    ResultSet mockResultSet;
 
     @Mock
     ResultSetMetaData mockResultSetMetaData;
@@ -83,6 +87,65 @@ public class DatabaseServiceTest {
     }
 
     @Test
+    public void executeParameterQueryTimestampTest() throws SQLException {
+        when(mockConnection.prepareStatement(SELECT_GENERIC_STATEMENT)).thenReturn(mockPreparedStatement);
+        Timestamp timestamp = new Timestamp(10000);
+        Object result = databaseEventService.executeParameterQuery(SELECT_GENERIC_STATEMENT, new Object[]{timestamp});
+        Assert.assertEquals("", result);
+    }
+
+    @Test
+    public void executeParameterQueryDateTest() throws SQLException {
+        when(mockConnection.prepareStatement(SELECT_GENERIC_STATEMENT)).thenReturn(mockPreparedStatement);
+        Date date = new Date(1000);
+        Object result = databaseEventService.executeParameterQuery(SELECT_GENERIC_STATEMENT, new Object[]{date});
+        Assert.assertEquals("", result);
+    }
+
+    @Test
+    public void executeParameterQueryUUIDTest() throws SQLException {
+        when(mockConnection.prepareStatement(SELECT_GENERIC_STATEMENT)).thenReturn(mockPreparedStatement);
+        UUID uuid = UUID.fromString("1-22-333-44-555");
+        Object result = databaseEventService.executeParameterQuery(SELECT_GENERIC_STATEMENT, new Object[]{uuid});
+        Assert.assertEquals("", result);
+    }
+
+    @Test
+    public void executeParameterQueryIntegerTest() throws SQLException {
+        when(mockConnection.prepareStatement(SELECT_GENERIC_STATEMENT)).thenReturn(mockPreparedStatement);
+        when(mockPreparedStatement.getResultSet()).thenReturn(mockResultSet);
+        when(mockResultSet.getMetaData()).thenReturn(mockResultSetMetaData);
+        Integer intVersion = new Integer("123");
+        List results = (List) databaseEventService.executeParameterQuery(SELECT_GENERIC_STATEMENT, new Object[]{intVersion});
+        Assert.assertEquals(1, results.size());
+        for (Object result : results) {
+            List columns = (List) result;
+            Assert.assertEquals(5, columns.size());
+            Assert.assertEquals(1, columns.get(0));
+            Assert.assertEquals("foobar", columns.get(1));
+            Assert.assertEquals("Anthony", columns.get(2));
+            Assert.assertEquals("San Diego", columns.get(3));
+            Assert.assertEquals("2 Oak Street", columns.get(4));
+        }
+    }
+
+    @Test
+    public void executeParameterQueryDoubleTest() throws SQLException {
+        when(mockConnection.prepareStatement(SELECT_GENERIC_STATEMENT)).thenReturn(mockPreparedStatement);
+        Double doubleVersion = new Double("123");
+        Object result = databaseEventService.executeParameterQuery(SELECT_GENERIC_STATEMENT, new Object[]{doubleVersion});
+        Assert.assertEquals("", result);
+    }
+
+    @Test
+    public void executeParameterQueryBigDecimalTest() throws SQLException {
+        when(mockConnection.prepareStatement(SELECT_GENERIC_STATEMENT)).thenReturn(mockPreparedStatement);
+        BigDecimal bd = new BigDecimal("123");
+        Object result = databaseEventService.executeParameterQuery(SELECT_GENERIC_STATEMENT, new Object[]{bd});
+        Assert.assertEquals("", result);
+    }
+
+    @Test
     public void executeQueryMongoTest() throws SQLException {
         BasicDBObject query = new BasicDBObject("docket", "1234");
         when(mockDB.getCollection("Patents")).thenReturn(mockDBCollection);
@@ -99,12 +162,41 @@ public class DatabaseServiceTest {
     }
 
     @Test
+    public void executeQuerySQLTemplateBadSQLTest() throws SQLException {
+        when(mockConnection.prepareStatement(SELECT_STATEMENT)).thenReturn(mockPreparedStatement);
+        Mockito.doThrow(new NullPointerException()).when(mockPreparedStatement).executeQuery();
+        ResultSet resultSet = databaseEventService.executeQuery(SELECT_STATEMENT);
+        Assert.assertNull(resultSet);
+    }
+
+    @Test
     public void executeParameterQueryResultSetTest() throws SQLException {
         when(mockConnection.prepareStatement(SELECT_STATEMENT)).thenReturn(mockPreparedStatement);
         Mockito.doReturn(mockResultSet).when(mockPreparedStatement).executeQuery();
         ResultSet resultSet = databaseEventService.executeQuery(SELECT_STATEMENT);
         Assert.assertEquals(1, resultSet.getInt("id"));
         Assert.assertEquals("foobar", resultSet.getString("patent_name"));
+    }
+
+    @Test
+    public void emptyResultSetExecuteParameterQueryLikeTest() throws SQLException {
+        when(mockConnection.prepareStatement(SELECT_LIKE_STATEMENT)).thenReturn(mockPreparedStatement);
+        Object result = databaseEventService.executeParameterQuery(SELECT_LIKE_STATEMENT, new Object[]{"Anthony"});
+        Assert.assertEquals("", result);
+    }
+
+    @Test
+    public void emptyResultSetExecuteParameterQueryObjectExceptionTest() throws SQLException {
+        when(mockConnection.prepareStatement(SELECT_STATEMENT)).thenThrow(new NullPointerException());
+        Object result = databaseEventService.executeParameterQuery(SELECT_STATEMENT, new Object[]{1L, "Anthony"});
+        Assert.assertEquals("", result);
+    }
+
+    @Test
+    public void emptyResultSetExecuteParameterQueryObjectIndexExceptionTest() throws SQLException {
+        when(mockConnection.prepareStatement(SELECT_STATEMENT)).thenThrow(new IndexOutOfBoundsException());
+        Object result = databaseEventService.executeParameterQuery(SELECT_STATEMENT, new Object[]{1L, "Anthony"});
+        Assert.assertEquals("", result);
     }
 
     @Test
