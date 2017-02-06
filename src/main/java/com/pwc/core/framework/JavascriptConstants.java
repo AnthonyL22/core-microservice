181a68fa2a22b58bffe23b85ab026d44a53b29bd
c-anlomb <c_anlomb@qualcomm.com>
2017-02-06 14:43:36 -0800

    Renamed SELECT_VALUE_ELEMENT_BY_XPATH element.

--- src/main/java/com/pwc/core/framework/JavascriptConstants.java
+++ src/main/java/com/pwc/core/framework/JavascriptConstants.java
@@ -2,20 +2,20 @@ package com.pwc.core.framework;
 
 public interface JavascriptConstants {
 
-    // Javascript Alerts
+    // Alert Actions
     String ALERT_BY_XPATH = "var element = document.evaluate(\"%s\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue; alert(element.textContent);";
 
-    // Javascript Other
+    // Other Actions
     String BLUR_ELEMENT_BY_XPATH = "(document.evaluate('%s', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;).blur();";
     String BLUR_ELEMENT_BY_ID = "document.getElementById('%s').blur();";
 
-    // Javascript Entry
+    // Text Entry Action
     String ENTER_VALUE_ELEMENT_BY_XPATH = "var element = document.evaluate(\"%s\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue; element.value='%s';";
 
-    // Javascript Drop Down
-    String SELECT_VALUE_ELEMENT_BY_XPATH = ENTER_VALUE_ELEMENT_BY_XPATH + " for(var i=0; i < element.options.length; i++){if(element.options[i].text === \"%s\") {element.selectedIndex = i; break;}};";
+    // SELECT Action
+    String SELECT_VALUE_BY_XPATH = ENTER_VALUE_ELEMENT_BY_XPATH + " for(var i=0; i < element.options.length; i++){if(element.options[i].text === \"%s\") {element.selectedIndex = i; break;}};";
 
-    // Javascript Clicks
+    // CLICK Action
     String CLICK_BY_XPATH = "var element = document.evaluate(\"%s\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue; element.click();";
     String CLICK_ELEMENT_BY_ID = "document.getElementById('%s').click();";
     String CLICK_BY_TAG_AND_VALUE = "var elements = document.getElementsByTagName('%s'); " +
@@ -35,7 +35,7 @@ public interface JavascriptConstants {
             "{if (elements[i].firstChild.textContent.indexOf('%s') > -1) elements[i].click();}";
     String CLICK_LAST_ELEMENT_BY_CLASS_NAME = "var elements = document.getElementsByClassName('%s'); elements[elements.length - 1].click();";
 
-    // Javascript Logical Queries
+    // Logical Queries
     String IS_JQUERY_AJAX_REQUESTS_ACTIVE = "if($.active == 0){ return true;} else { return false;}";
     String IS_DOCUMENT_READY = "if(document.readyState == 'complete'){ return true;} else { return false;}";
     String IS_OPEN_HTTPS = "return window.openHTTPs";
