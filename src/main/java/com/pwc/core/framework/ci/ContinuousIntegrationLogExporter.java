ccc3497bc44799073145af71b66dc4817d1ea155
Anastasiya <59669121+AKirylava@users.noreply.github.com>
2020-01-22 15:51:30 -0800

    added new word export capability (#15)

--- src/main/java/com/pwc/core/framework/ci/ContinuousIntegrationLogExporter.java
+++ src/main/java/com/pwc/core/framework/ci/ContinuousIntegrationLogExporter.java
@@ -2,110 +2,79 @@ package com.pwc.core.framework.ci;
 
 import com.pwc.core.framework.FrameworkConstants;
 import com.pwc.logging.helper.LoggerHelper;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.SuffixFileFilter;
 import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.pwc.logging.service.LoggerService.LOG;
+
 
 public class ContinuousIntegrationLogExporter {
 
     private static Map constants;
+    private static XWPFDocument document;
+    private static String testNgGroupClassName;
+    private static String testNgGroupName;
+    private static final String GHERKIN_REGEX = "FEATURE\\(|SCENARIO\\(|GIVEN\\(|WHEN\\(|THEN\\(|AND\\(|BUT\\(|OR\\(|IF\\(|NOT\\(|FINALLY\\(";
+    private static final int EXPECTED_PARAMETER_COUNT = 5;
 
     /**
      * Maven utility to transform class that has all TestNG groups defined into a comma separated list that can be
      * used in Jenkins jobs.
      *
-     * @param args application arguements
-     * @throws Exception
+     * @param args application arguments
+     * @throws Exception missing information to run the application
      */
     public static void main(String[] args) throws Exception {
 
-        if (args.length < 3) {
-            throw new Exception("Missing required arguments; " +
-                    "[0] = Source Test Directory (ex: src.test.java.com.google.automation.tests), " +
-                    "[1] = Source Main Directory (ex: src.main.java.com.google.automation.framework), " +
-                    "[2] = Manual Test Report Name (ex: Manual_Test_Report.txt), " +
-                    "[3...] = OPTIONAL - Classes that contain Constant 'Name=Value' pairs");
+        if (args.length < EXPECTED_PARAMETER_COUNT) {
+
+            StringBuilder exception = new StringBuilder("Missing required arguments;");
+            exception.append("[0] = Source Test Directory (ex: src.test.java.com.google.automation.tests)");
+            exception.append(", \r\n");
+            exception.append("[1] = Source Main Directory (ex: src.main.java.com.google.automation.framework)");
+            exception.append(", \r\n");
+            exception.append("[2] = Manual Test Report Name (ex: Manual_Test_Report.txt | .doc)");
+            exception.append(", \r\n");
+            exception.append("[3] = Class that contain TestNG Group identifiers (ex. Groups.java))");
+            exception.append(", \r\n");
+            exception.append("[4] = Specific group name to process ignoring all others");
+            throw new Exception(exception.toString());
+
         } else {
 
             StringBuilder sourceTestDirectory = getJavaDirectory(args[0]);
-
             File reportFile = new File(args[2]);
             if (reportFile.exists()) {
                 reportFile.delete();
             }
-
-            List<String> constantFiles = new ArrayList<>();
-            if (args.length > 3) {
-                for (int i = 3; i < args.length; i++) {
-                    constantFiles.add(args[i]);
-                }
-                setConstants(getConstantValues(args[1], constantFiles));
-            }
-
+            testNgGroupClassName = StringUtils.substringBefore(args[3], ".");
+            testNgGroupName = args[4].toUpperCase();
             List<File> files = generateManualTestOutput(sourceTestDirectory.toString(), reportFile);
-            appendStatisticsToReport(reportFile, files);
+            generateReportWithStatistics(reportFile, files);
 
         }
 
     }
 
     /**
-     * Get constant values for a given Interface file.
-     *
-     * @param directory     directory where TestNG group interface resides
-     * @param constantFiles list of .java files that contain name/value pairs (ex: Constants.java)
-     * @return full path to source directory
-     */
-    protected static Map getConstantValues(final String directory, final List<String> constantFiles) {
-
-        StringBuilder source = getJavaDirectory(directory);
-        Map constantMap = new HashMap();
-
-        try {
-
-            for (String fileName : constantFiles) {
-
-                List<File> fileToScan = (List<File>) FileUtils.listFiles(new File(source.toString()), new SuffixFileFilter(fileName), TrueFileFilter.INSTANCE);
-
-                for (File file : fileToScan) {
-
-                    List<String> readingLines = FileUtils.readLines(file, Charset.defaultCharset());
-                    for (String readLine : readingLines) {
-                        if (StringUtils.contains(readLine, "=")) {
-                            readLine = readLine.trim();
-                            String left = StringUtils.substring(readLine, StringUtils.indexOf(readLine, " "), StringUtils.indexOf(readLine, "=")).trim();
-                            String right = StringUtils.substring(readLine, StringUtils.indexOf(readLine, "= ") + 1, StringUtils.indexOf(readLine, ";")).trim();
-                            right = StringUtils.replace(right, "\"", "");
-                            constantMap.put(left, right);
-                        }
-                    }
-                }
-
-            }
-
-        } catch (IOException e) {
-            LOG(true, "Error processing constant file(s)");
-        }
-
-        return constantMap;
-
-    }
-
-    /**
-     * Generate manual test output report
+     * Generate manual test output report.
      *
      * @param sourceTestFileDirectory source file directory
      * @param testReportFile          destination report File
@@ -116,67 +85,41 @@ public class ContinuousIntegrationLogExporter {
         List<File> files = (List<File>) FileUtils.listFiles(new File(sourceTestFileDirectory), new SuffixFileFilter("Test.java"), TrueFileFilter.INSTANCE);
 
         try {
+            boolean matchesDesiredGroup = false;
             for (File file : files) {
-
                 List<String> readingLines = FileUtils.readLines(file, Charset.defaultCharset());
                 List<String> writeLines = new ArrayList<>();
-
                 for (String readLine : readingLines) {
-                    if (StringUtils.contains(readLine, "FEATURE(")) {
-                        writeLines.add(LoggerHelper.formatGherkinMessage("Feature: " + StringUtils.substringBetween(processArguments(readLine), "\"", "\"")));
+                    if (StringUtils.containsIgnoreCase(readLine, String.format("%s.%s", testNgGroupClassName, testNgGroupName))) {
+                        matchesDesiredGroup = true;
                     }
-
-                    if (StringUtils.contains(readLine, "SCENARIO(")) {
-                        writeLines.add(LoggerHelper.formatGherkinMessage("Scenario: " + StringUtils.substringBetween(processArguments(readLine), "\"", "\"")));
+                    if (StringUtils.substringBefore(readLine, "\"").trim().matches(GHERKIN_REGEX) && matchesDesiredGroup) {
+                        Pattern pattern = Pattern.compile(GHERKIN_REGEX);
+                        Matcher matcher = pattern.matcher(readLine);
+                        if (matcher.find()) {
+                            String result = matcher.group(0);
+                            if (result.contains("FEATURE(") || result.contains("SCENARIO(") || result.contains("GIVEN(")) {
+                                writeLines.add(LoggerHelper.formatGherkinMessage(result.substring(0, 1).toUpperCase() + result.substring(1).toLowerCase().replace("(", ": ")
+                                                + StringUtils.substringBetween(processArguments(readLine), "\"", "\"")));
+                            } else {
+                                writeLines.add(LoggerHelper.formatGherkinMessage(result.substring(0, 1).toUpperCase() + result.substring(1).toLowerCase().replace("(", " ")
+                                                + StringUtils.substringBetween(processArguments(readLine), "\"", "\"")));
+                            }
+                        }
                     }
-
-                    if (StringUtils.contains(readLine, "GIVEN(")) {
-                        writeLines.add(LoggerHelper.formatGherkinMessage("Given " + StringUtils.substringBetween(processArguments(readLine), "\"", "\"")));
-                    }
-
-                    if (StringUtils.contains(readLine, "WHEN(")) {
-                        writeLines.add(LoggerHelper.formatGherkinMessage("When " + StringUtils.substringBetween(processArguments(readLine), "\"", "\"")));
-                    }
-
-                    if (StringUtils.contains(readLine, "THEN(")) {
-                        writeLines.add(LoggerHelper.formatGherkinMessage("Then " + StringUtils.substringBetween(processArguments(readLine), "\"", "\"")));
-                    }
-
-                    if (StringUtils.contains(readLine, "AND(")) {
-                        writeLines.add(LoggerHelper.formatGherkinMessage("And " + StringUtils.substringBetween(processArguments(readLine), "\"", "\"")));
-                    }
-
-                    if (StringUtils.contains(readLine, "BUT(")) {
-                        writeLines.add(LoggerHelper.formatGherkinMessage("But " + StringUtils.substringBetween(processArguments(readLine), "\"", "\"")));
-                    }
-
-                    if (StringUtils.contains(readLine, "OR(")) {
-                        writeLines.add(LoggerHelper.formatGherkinMessage("Or " + StringUtils.substringBetween(processArguments(readLine), "\"", "\"")));
-                    }
-
-                    if (StringUtils.contains(readLine, "IF(")) {
-                        writeLines.add(LoggerHelper.formatGherkinMessage("If " + StringUtils.substringBetween(processArguments(readLine), "\"", "\"")));
-                    }
-
-                    if (StringUtils.contains(readLine, "NOT(")) {
-                        writeLines.add(LoggerHelper.formatGherkinMessage("Not " + StringUtils.substringBetween(processArguments(readLine), "\"", "\"")));
-                    }
-
-                    if (StringUtils.contains(readLine, "FINALLY(")) {
-                        writeLines.add(LoggerHelper.formatGherkinMessage("Finally " + StringUtils.substringBetween(processArguments(readLine), "\"", "\"")));
-                    }
-
-                    if (StringUtils.contains(readLine, "IMAGE(")) {
-                        writeLines.add(LoggerHelper.formatGherkinMessage(StringUtils.substringBetween(processArguments(readLine), "\"", "\"")));
-                    }
-
                 }
 
-                writeLines.add(org.apache.commons.lang.StringUtils.repeat("-", 60));
-                writeLines.add("\n");
+                if (matchesDesiredGroup) {
+                    matchesDesiredGroup = false;
+                    writeLines.add(org.apache.commons.lang.StringUtils.repeat("-", 60));
+                    writeLines.add("\n");
 
-                FileUtils.writeLines(testReportFile, writeLines, true);
-
+                    if (StringUtils.containsIgnoreCase(testReportFile.getName(), ".doc")) {
+                        writeWordDoc(testReportFile.getName(), writeLines);
+                    } else {
+                        FileUtils.writeLines(testReportFile, writeLines, true);
+                    }
+                }
             }
         } catch (IOException e) {
             LOG(true, "Error generating manual test report");
@@ -191,7 +134,7 @@ public class ContinuousIntegrationLogExporter {
      * @param outputFile report file
      * @param files      files to process
      */
-    protected static void appendStatisticsToReport(final File outputFile, final List<File> files) {
+    protected static void generateReportWithStatistics(final File outputFile, final List<File> files) {
 
         try {
             List<String> adminLines = new ArrayList<>();
@@ -199,27 +142,51 @@ public class ContinuousIntegrationLogExporter {
             adminLines.add("MANUAL TEST CASE REPORT SUMMARY");
             adminLines.add(org.apache.commons.lang.StringUtils.repeat("-", 60));
             adminLines.add(files.size() + " Tests Processed");
-            adminLines.add("Packages Processed:");
 
-            Collection<String> uniquePackageNames = new LinkedHashSet<>();
-
-            for (File file : files) {
-                List<String> readingLines = FileUtils.readLines(file, Charset.defaultCharset());
-                for (String readLine : readingLines) {
-                    if (StringUtils.contains(readLine, "package")) {
-                        uniquePackageNames.add("  " + readLine);
-                    }
-                }
+            if (StringUtils.containsIgnoreCase(outputFile.getName(), ".doc")) {
+                writeWordDoc(outputFile.getName(), adminLines);
+            } else {
+                FileUtils.writeLines(outputFile, adminLines, true);
             }
-
-            for (String packageName : uniquePackageNames) {
-                adminLines.add(packageName);
-            }
-
-            FileUtils.writeLines(outputFile, adminLines, true);
-
         } catch (IOException e) {
             LOG(true, "Error processing statistics report");
+        }
+    }
+
+    /**
+     * Process all logging into a Word document.
+     *
+     * @param filename Word document to create
+     * @param lines    all lines to write to document
+     */
+    private static void writeWordDoc(final String filename, final List<String> lines) {
+
+        try {
+            if (null != document) {
+                XWPFParagraph tmpParagraph = document.createParagraph();
+                XWPFRun wordRun = tmpParagraph.createRun();
+                wordRun.setFontSize(10);
+                lines.forEach(line -> {
+                    wordRun.setText(line);
+                    wordRun.addCarriageReturn();
+                });
+
+            } else {
+                document = new XWPFDocument();
+                XWPFParagraph tmpParagraph = document.createParagraph();
+                XWPFRun wordRun = tmpParagraph.createRun();
+                wordRun.setFontSize(10);
+                lines.forEach(line -> {
+                    wordRun.setText(line);
+                    wordRun.addCarriageReturn();
+                });
+
+            }
+            FileOutputStream fos = new FileOutputStream(new File(filename));
+            document.write(fos);
+            fos.close();
+        } catch (IOException e) {
+            LOG(true, "Error processing statistics Word Document report");
         }
     }
 
@@ -247,7 +214,7 @@ public class ContinuousIntegrationLogExporter {
                 trimmedList[i] = temp[i].trim();
             }
 
-            if (null != trimmedList) {
+            if (CollectionUtils.isEmpty(Arrays.asList(trimmedList))) {
                 replaceAllConstantsWithMapValues(trimmedList);
                 return LoggerHelper.formatMessage(readLine, trimmedList);
             }
