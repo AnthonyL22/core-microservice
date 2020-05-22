aad630fc7bcbe14280f642b915e66173dc342a01
anthonyl22 <anthonyl22@gmail.com>
2020-05-22 09:08:40 -0700

    Fixed more formatting issues.  Code is now spotless :)

--- src/main/java/com/pwc/core/framework/data/Credentials.java
+++ src/main/java/com/pwc/core/framework/data/Credentials.java
@@ -2,8 +2,10 @@ package com.pwc.core.framework.data;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class Credentials {
 
--- src/main/java/com/pwc/core/framework/data/HeaderKeysMap.java
+++ src/main/java/com/pwc/core/framework/data/HeaderKeysMap.java
@@ -2,10 +2,12 @@ package com.pwc.core.framework.data;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.HashMap;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class HeaderKeysMap {
 
